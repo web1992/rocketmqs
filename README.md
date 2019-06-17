9255aeb84298146b3e4ab6ea82bbf81453a00bde
erbao.wang <554933654@qq.com>
2019-06-17 22:04:37 +0800

    add demo

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # rocketmqs
 
 - [quick-start](https://rocketmq.apache.org/docs/quick-start/)
+- [example](https://github.com/apache/rocketmq/tree/master/example)
--- rocketmq-demo/src/main/java/cn/web1992/rocketmq/simple/OnewayProducer.java
+++ rocketmq-demo/src/main/java/cn/web1992/rocketmq/simple/OnewayProducer.java
@@ -1,15 +1,13 @@
-package cn.web1992.rocketmq;
+package cn.web1992.rocketmq.simple;
 
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
-import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
 
-public class SyncProducer {
+public class OnewayProducer {
     public static void main(String[] args) throws Exception {
         //Instantiate with a producer group name.
-        DefaultMQProducer producer = new
-                DefaultMQProducer("please_rename_unique_group_name");
+        DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
         // Specify name server addresses.
         producer.setNamesrvAddr("localhost:9876");
         //Launch the instance.
@@ -22,8 +20,8 @@ public class SyncProducer {
                             i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
             );
             //Call send message to deliver message to one of brokers.
-            SendResult sendResult = producer.send(msg);
-            System.out.printf("%s%n", sendResult);
+            producer.sendOneway(msg);
+
         }
         //Shut down once the producer instance is not longer in use.
         producer.shutdown();
