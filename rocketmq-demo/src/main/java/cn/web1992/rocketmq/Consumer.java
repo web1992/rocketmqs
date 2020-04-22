a444a7eca7328eb8c15c697e86953f12f8e96677
web1992 <554933654@qq.com>
2020-04-22 20:58:37 +0800

    fix mq test

--- rocketmq-demo/src/main/java/cn/web1992/rocketmq/Consumer.java
+++ rocketmq-demo/src/main/java/cn/web1992/rocketmq/Consumer.java
@@ -1,4 +1,4 @@
-package cn.web1992.rocketmq.simple;
+package cn.web1992.rocketmq;
 
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
@@ -15,10 +15,10 @@ public class Consumer {
 
         // Instantiate with specified consumer group name.
         DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("please_rename_unique_group_name");
-         
+
         // Specify name server addresses.
         consumer.setNamesrvAddr("localhost:9876");
-        
+
         // Subscribe one more more topics to consume.
         consumer.subscribe("TopicTest", "*");
         // Register callback to execute on arrival of messages fetched from brokers.
--- rocketmq-demo/src/main/java/cn/web1992/rocketmq/simple/AsyncProducer.java
+++ rocketmq-demo/src/main/java/cn/web1992/rocketmq/simple/AsyncProducer.java
@@ -16,22 +16,23 @@ public class AsyncProducer {
         producer.start();
         producer.setRetryTimesWhenSendAsyncFailed(0);
         for (int i = 0; i < 100; i++) {
-                final int index = i;
-                //Create a message instance, specifying topic, tag and message body.
-                Message msg = new Message("TopicTest",
+            final int index = i;
+            //Create a message instance, specifying topic, tag and message body.
+            Message msg = new Message("TopicTest",
                     "TagA",
                     "OrderID188",
                     "Hello world".getBytes(RemotingHelper.DEFAULT_CHARSET));
-                producer.send(msg, new SendCallback() {
-                    public void onSuccess(SendResult sendResult) {
-                        System.out.printf("%-10d OK %s %n", index,
+            producer.send(msg, new SendCallback() {
+                public void onSuccess(SendResult sendResult) {
+                    System.out.printf("%-10d OK %s %n", index,
                             sendResult.getMsgId());
-                    }
-                    public void onException(Throwable e) {
-                        System.out.printf("%-10d Exception %s %n", index, e);
-                        e.printStackTrace();
-                    }
-                });
+                }
+
+                public void onException(Throwable e) {
+                    System.out.printf("%-10d Exception %s %n", index, e);
+                    e.printStackTrace();
+                }
+            });
         }
         //Shut down once the producer instance is not longer in use.
         producer.shutdown();
